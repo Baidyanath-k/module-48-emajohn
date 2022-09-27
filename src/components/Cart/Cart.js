3b2a5dbd3b69957992eddebe7f527d0be1244d9c
Baidyanath-K <baidyanathkumar12091998@gmail.com>
2022-09-28 01:26:37 +0600

    add cart details

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import'./Cart.css';
 
 const Cart = ({cart}) => {
     return (
-        <div>
+        <div className='cart'>
             <h2>Order Summery</h2>
             <h4>Selected Items: {cart.length}</h4>
+            <p>Total Price: $</p>
+            <p>Total Shiping Charge: $</p>
+            <p>Tax: $</p>
+            <h4>Grand Total: $</h4>
         </div>
     );
 };
